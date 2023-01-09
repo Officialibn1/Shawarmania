584e0ecd57237ece304c4c99c70f6671d1c63838
Officialibn1 <officialibn001@gmail.com>
2023-01-09 15:24:25 +0100

    Created a counter app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-
-import './App.css';
+import { useState } from "react";
+import Content from "./Components/Content";
+import Nav from "./Components/Nav";
 
 function App() {
+  //Javascript Functions 
+  const [counter, setCounter] = useState(0)
+  const [toggle, setToggle] = useState(false)
+
+  //Counter Function
+  const incCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  const decCounter = () => {
+    setCounter(counter === 0 ? 0 : counter - 1)
+  }
+
+  //Toggle Function
+  const Toggle = () => {
+    setToggle((prev) => !prev)
+  }
+
   return (
     <div className="App">
-      <h1>Hello React Props</h1>
+      <Nav Toggle={Toggle} />
+
+      <div className="Div">
+        <Content counter={counter} incCounter={incCounter} decCounter={decCounter} toggle={toggle} />
+      </div>
     </div>
   );
 }
