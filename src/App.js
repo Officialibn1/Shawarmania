c75a2eaaacbdf6b2e73dc641ede0f3e4fe5daaa0
Officialibn1 <officialibn001@gmail.com>
2023-01-10 04:40:54 +0100

    Cleaned Up the Counter App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import { useState } from "react";
-import Content from "./Components/Content";
-import Nav from "./Components/Nav";
+
 
 function App() {
-  //Javascript Functions 
-  const [counter, setCounter] = useState(0)
-  const [toggle, setToggle] = useState(false)
-
-  //Counter Function
-  const incCounter = () => {
-    setCounter(counter + 1)
-  }
-
-  const decCounter = () => {
-    setCounter(counter === 0 ? 0 : counter - 1)
-  }
-
-  //Toggle Function
-  const Toggle = () => {
-    setToggle((prev) => !prev)
-  }
 
   return (
     <div className="App">
-      <Nav Toggle={Toggle} />
-
-      <div className="Div">
-        <Content counter={counter} incCounter={incCounter} decCounter={decCounter} toggle={toggle} />
-      </div>
+      <h1>Hello New Project</h1>
     </div>
   );
 }
