187a6faad3bc368d411ec0f67c3a5cbc1970acbd
Officialibn1 <officialibn001@gmail.com>
2023-01-10 05:09:15 +0100

    Deleted the Counter App Again

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import Counter from "./Components/Counter";
-import Nav from "./Components/Nav";
-import { useState } from "react";
-
 
 function App() {
-// Function for the App
-  const [toggle, setToggle] = useState(false)
-  const [counter, setCounter] = useState(0)
-
-// Toggle Function
-  const toggler = () => {
-    setToggle((prev) => prev = !prev)
-  }
-
-// Counter Function
-
-  // Increment
-  const increment = () => {
-    setCounter(counter + 1)
-  }
-
-  // Decrement
-  const decrement = () => {
-    setCounter(counter === 0 ? 0 : counter - 1)
-  }
 
 
   return (
     <div className="App">
-      <Nav toggler={toggler} />
-      <Counter toggle={toggle} increment={increment}
-      decrement={decrement} counter={counter} />
+      <h1 className="H1">React Practice</h1>
     </div>
   );
 }
