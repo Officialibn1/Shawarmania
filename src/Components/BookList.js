99cbeaea343afd5f89a529603e75f01deedfb9ff
Officialibn1 <officialibn001@gmail.com>
2023-01-29 16:57:18 +0100

    styled link

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -5,12 +5,14 @@ const BookList = () => {
     return (
         <>
             <h1>
-                BookList 
+                BookList
+                <nav> 
                 <ul>
                     <li><Link to='/books/1'>Book 1</Link></li>
                     <li><Link to='/books/2'>Book 2</Link></li>
                     <li><Link to='/books/New'>New Book</Link></li>
                 </ul>
+                </nav>
             </h1>
         </>
     );
