9e8934b2e2501f48f8e1d340add5951c523ce8e8
Officialibn1 <officialibn001@gmail.com>
2023-01-29 16:14:11 +0100

    progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Home from './Components/Home';
+import BookList from './Components/BookList';
 import Book from './Components/Book';
 function App() {
 
 
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path="/books" element={<Book />} />
-    </Routes>
+    <div className="Div">
+      <div className="content" >
+        <nav>
+          <ul>
+            <li><Link to='/' >Home</Link></li>
+            <li><Link to='/books' >BookList</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/books" element={<BookList />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
