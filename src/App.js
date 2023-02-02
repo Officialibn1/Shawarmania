3daa8dfe0ab63ad9080a95888ea659261cdb7f9a
Officialibn1 <officialibn001@gmail.com>
2023-02-02 06:31:17 +0100

    created some components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import Comments from "./Components/Comments";
+import Post from "./Components/Post";
+import Users from "./Components/Users";
 
 function App() {
+  const [resourceType, setResourceType] = useState(<Post />);
+
 
   return (
     <div className="Div">
       <div className="content" >
-        <h1>Hello useEffect</h1>
+
+        <button className="Button" onClick={() => setResourceType(<Post />)}>Post</button>
+        <button className="Button" onClick={() => setResourceType(<Users />)}>Users</button>
+        <button className="Button" onClick={() => setResourceType(<Comments />)}>Comments</button>
+        <h1>{resourceType}</h1>
       </div>
     </div>
   );
---
+++ src/Components/Comments.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Comments = () => {
+    return (
+        <>
+            Comments
+        </>
+    );
+}
+
+export default Comments;
