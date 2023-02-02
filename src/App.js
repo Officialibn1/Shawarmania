6366a6eef5637a3502fdb68a2859df75df98edd4
Officialibn1 <officialibn001@gmail.com>
2023-02-02 12:30:58 +0100

    useEffect lesson completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import { useEffect, useState } from "react";
-import Comments from "./Components/Comments";
-import Post from "./Components/Post";
-import Users from "./Components/Users";
+
 
 function App() {
-  const [resourceType, setResourceType] = useState(<Post />);
 
+  // useState and useEffect for handling text and resource type
+  
+  const [resourceType, setResourceType] = useState('posts');
+  const [text, setText] = useState([]);
   useEffect(() => {
-    console.log('Effect Rendered')
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setText(json))
     return () => {
-      console.log('Effect RenderCleaned Up')
+      ''
     };
   }, [resourceType]);
+
+  // --------------------------------------------------
+
+  // useState & useEffect for handling windows resize
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const setWidth = () => {
+    setWindowWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', setWidth)
+    return () => {
+      window.removeEventListener('resize', setWidth)
+    }
+  }, []);
+
+  // ----------------------------------------------------
 
   return (
     <div className="Div">
       <div className="content" >
 
-        <button className="Button" onClick={() => setResourceType(<Post />)}>Post</button>
-        <button className="Button" onClick={() => setResourceType(<Users />)}>Users</button>
-        <button className="Button" onClick={() => setResourceType(<Comments />)}>Comments</button>
+        <button className="Button" onClick={() => setResourceType('posts')}>Post</button>
+        <button className="Button" onClick={() => setResourceType('users')}>Users</button>
+        <button className="Button" onClick={() => setResourceType('comments')}>Comments</button>
         <h1>{resourceType}</h1>
+        <div>
+          {windowWidth}
+        </div>
       </div>
     </div>
   );
