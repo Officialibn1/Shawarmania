00a04760dfead1434c910a2a154b8d55b0b96e50
Officialibn1 <officialibn001@gmail.com>
2023-02-03 15:35:48 +0100

    clean up useMemo codes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
-import { useState, useMemo } from "react";
 
 function App() {
-  const [number, setNumber] = useState(0);
-
-  const [dark, setDark] = useState(false);
-  
-  // UseMemo for Theme Styles
-
-  const themeMode = useMemo(() => {
-    return {
-      backgroundColor: dark ? 'black' : 'white',
-      color: dark ? 'white' : 'black',
-      margin: '20px',
-      borderRadius: '5px'
-  }}, [dark]) 
-  
-  // -------------------------------------------------------
-
-
-  // UseMemo and DoubleNumber Function for memorizing the number state
-  
-  const doubleNumber = useMemo(() => {
-    return slowFunction(number);
-  }, [number]) 
-
-  // ------------------------------------------------------------
-
 
   return (
     <div className="Div">
       <div className="content">
-        <input className="Button" type="number" value={number} onChange={(e) => setNumber(parseInt(e.target.value))}/>
-        <button className="Button" onClick={() => setDark(prevDark => !prevDark)}>{dark ? 'Light Mode' : 'Dark Mode'}</button>
-        <h1 style={themeMode} >{doubleNumber}</h1>
+        <h1>Hello Reducer</h1>
       </div>
     </div>
   );
-}
-
-const slowFunction = (number) => {
- for (let i = 0; i <= 300000000; i++) {}
-  return number * 2 
 }
 
 export default App;
