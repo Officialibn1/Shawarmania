62f3d02d8dfd179ebb259b851214f74aa709de71
Officialibn1 <officialibn001@gmail.com>
2023-02-20 21:59:42 +0100

    nav links adjusted to a tags

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
         <nav className='navbar navbar-expand-lg navbar-dark bg-dark border border-bottom border-alert fixed-top'>
             <div className="container">
                 {/* Brand Logo */}
-                <Link to='#' className='navbar-brand'>
+                <Link to='/' className='navbar-brand'>
                     <h2 className="fw-light text-warning">
                         Shawarmania
                     </h2>
@@ -20,11 +20,11 @@ const Nav = () => {
                 {/* Navbar Links */}
                 <div id='navMenu' className="collapse navbar-collapse text-sm-center">
                     <ul className="navbar-nav ms-auto">
-                        <li className="nav-item active"><Link className='nav-link' to="#home">Home</Link></li>
-                        <li className="nav-item"><Link className='nav-link' to="#subscribe">Subscribe</Link></li>
-                        <li className="nav-item"><Link className='nav-link' to="/order">Order</Link></li>
-                        <li className="nav-item"><Link className='nav-link' to="/showcase">Showcase</Link></li>
-                        <li className="nav-item"><Link className='nav-link' to="/about">About</Link></li>
+                        <li className="nav-item active"><a className='nav-link' href="#home">Home</a></li>
+                        <li className="nav-item"><a className='nav-link' href="#subscribe">Subscribe</a></li>
+                        <li className="nav-item"><a className='nav-link' href="#order">Order</a></li>
+                        <li className="nav-item"><a className='nav-link' href="#showcase">Showcase</a></li>
+                        <li className="nav-item"><a className='nav-link' href="#about">About</a></li>
                     </ul>
                 </div>
             </div>
--- src/Components/Subscribe.jsx
+++ src/Components/Subscribe.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Subscribe = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="bg-light p-5" id='subscribe'>
+            <div className="container d-lg-flex justify-content-between">
+                <h2 className='text-warning fw-light'>Subscribe for Deals & Promo New's</h2>
+                <div className="input-group inputGroup">
+                    <input type="email" className="form-control" placeholder='Enter Your Email...' />
+                    <button className="btn btn-dark">Send</button>
+                </div>
+            </div>
+        </section>
     );
 }
 
