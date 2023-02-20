c701db835738b073559cd9dd5e5fe005164b9ffa
Officialibn1 <officialibn001@gmail.com>
2023-02-20 21:35:25 +0100

    adjusted header component classnames

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
     return (
         <section className='p-5 bg-dark text-light' id='#home'>
             <div className="container mt-5">
-                <div className="row align-items-center justify-content-center">
-                    <div className="text-md-start text-sm-center col-md-6">
+                <div className="d-lg-flex align-items-center justify-content-center">
+                    <div className="text-md-start text-sm-center ">
                         <h2 className='text-warning'>Shawarmania Fast Foods, Snack and Chups</h2>
                         <p className="lead">
                             We offer you the best and delicious meals you can get in town, all your favourite chups, snacks, pizza's, KFC standard prepared chicken and much more...
@@ -14,8 +14,8 @@ const Header = () => {
                             Order Now
                         </button>
                     </div>
-                    <div className='col-md-6'>
-                        <img src="./images/mockup1.png" alt="" className="img img-fluid mt-md-5 mx-auto" />
+                    <div className='container d-flex align-items-center justify-content-center'>
+                        <img src="./images/mockup1.png" alt="" className="img img-fluid mt-md-5" />
                     </div>
                 </div>
             </div>
