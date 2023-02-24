5b01b1927eb6fe3c526fa43c4600d46eb4e7e337
Officialibn1 <officialibn001@gmail.com>
2023-02-24 18:45:39 +0100

    modified css file

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <nav className='navbar navbar-expand-lg navbar-dark bg-dark border border-bottom border-alert fixed-top'>
+        <nav className='navbar navbar-expand-lg navbar-dark bg-dark fixed-top'>
             <div className="container">
                 {/* Brand Logo */}
                 <Link to='/' className='navbar-brand'>
-                    <img src="./images/shawarmania.png" alt="" />
+                    <img src="./images/shawarmania.png" alt="Logo"  className='CustomLogo' />
                 </Link>
 
                 {/* Toggle Icon */}
@@ -19,7 +19,7 @@ const Nav = () => {
                 <div id='navMenu' className="collapse navbar-collapse text-sm-center">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item active"><a className='nav-link' href="#home">Home</a></li>
-                        <li className="nav-item"><a className='nav-link' href="#subscribe">Subscribe</a></li>
+                        <li className="nav-item"><a className='nav-link' href="#subscribe">Services</a></li>
                         <li className="nav-item"><a className='nav-link' href="#order">Order</a></li>
                         <li className="nav-item"><a className='nav-link' href="#showcase">Showcase</a></li>
                         <li className="nav-item"><a className='nav-link' href="#about">About</a></li>
